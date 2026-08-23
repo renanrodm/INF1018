f7fe4cfdb2e24311460bde4ab2b8b4e83cac095a
Renan Martins <renanrodm@gmail.com>
2026-08-23 23:27:26 +0000

    Modificado a função para suportar bases entre 2 e 10

--- 2026-08-18-aula-3/lab2/string2num.c
+++ 2026-08-18-aula-3/lab2/string2num.c
@@ -1,16 +1,18 @@
 #include <ctype.h>
 #include <stdio.h>
 
-int string2num (char *s) {
+int string2num (char *s, int base) {
   int a = 0;
   for (; *s; s++)
-    a = a*10 + (*s - '0');
+    a = a*base + (*s - '0');
   return a;
 }
 
 int main (void) {
-  printf("==> %d\n", string2num("1234"));
-  printf("==> %d\n", string2num("1234") + 1);
-  printf("==> %d\n", string2num("1234") + string2num("1"));
+  printf("==> %d\n", string2num("1234", 10));
+  printf("==> %d\n", string2num("1234", 10) + 1);
+  printf("==> %d\n", string2num("1234", 10) + string2num("1", 10));
+  printf("%d\n", string2num("777", 8));
+  printf("%d\n", string2num("777", 10));
   return 0;
 }
