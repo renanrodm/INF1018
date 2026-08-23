f3b64ba3eae75f2ccaebe88cbc41a32b81f64a5d
Renan Martins <renanrodm@gmail.com>
2026-08-23 23:33:24 +0000

    Response a pergunta c
    Signed-off-by: Renan Martins <renanrodm@gmail.com>

--- 2026-08-18-aula-3/lab2/string2num.c
+++ 2026-08-18-aula-3/lab2/string2num.c
@@ -3,8 +3,15 @@
 
 int string2num (char *s, int base) {
   int a = 0;
-  for (; *s; s++)
+  int dig;
+  for (; *s; s++){
+    if(isdigit(*s))
+      dig = *s - '0';
+    else
+      dig = *s - 'a' + 10;
+
     a = a*base + (*s - '0');
+  }
   return a;
 }
 
@@ -14,5 +21,8 @@ int main (void) {
   printf("==> %d\n", string2num("1234", 10) + string2num("1", 10));
   printf("%d\n", string2num("777", 8));
   printf("%d\n", string2num("777", 10));
+  printf("%d\n", string2num("1a", 16));
+  printf("%d\n", string2num("a09b", 16));
+  printf("%d\n", string2num("z09b", 36));
   return 0;
 }
