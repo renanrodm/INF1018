03bf835604b64f1280afe1fffa31edce79754b21
Renan Martins <renanrodm@gmail.com>
2026-09-15 14:06:35 +0000

    extra_lab7

--- 2026-09-03-aula-08-intro-ling-montagem/lab6/prog1.s
+++ 2026-09-03-aula-08-intro-ling-montagem/lab6/prog1.s
@@ -9,10 +9,15 @@ int main() {
 */
 
 /*
-Dicionario
-Reg   Var 
-EBx   i
-r12 p
+int nums[] = {10, -21, -30, 45};
+int main() {
+  int i, *p;
+  int sum = 0;
+  for (i = 0, p = nums; i != 4; i++, p++)
+    sum += *p;
+  printf("%d\n", sum);
+  return 0;
+}
 */
 
 .data
