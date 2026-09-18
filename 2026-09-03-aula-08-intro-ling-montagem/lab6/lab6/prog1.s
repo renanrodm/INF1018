84151775d373945db52684650d848d1a64458b96
Renan Martins <renanrodm@gmail.com>
2026-09-18 18:54:37 +0000

    LAB6_Q3: Corrige variavel desnecessaria

--- 2026-09-03-aula-08-intro-ling-montagem/lab6/lab6/prog1.s
+++ 2026-09-03-aula-08-intro-ling-montagem/lab6/lab6/prog1.s
@@ -3,8 +3,7 @@ int nums[] = {10, -21, -30, 45};
 int main() {
   int i, *p;
   for (i = 0, p = nums; i != 4; i++, p++)
-    if ((*p % 2) == 0)
-      printf("%d\n", *p);
+    printf("%d\n", *p);
   return 0;
 }
 */
@@ -26,34 +25,27 @@ main:
   movq    %r12, -16(%rbp)
 /********************************************************/
 
-  movl  $0, %ebx  /* i (ebx) = 0*/
-  movq  $nums, %r12  /* p (%r12) = &nums*/
+  movl  $0, %ebx  /* ebx = 0; */
+  movq  $nums, %r12  /* r12 = &nums */
 
 L1:
   cmpl  $4, %ebx  /* if (ebx == 4) ? */
   je  L2          /* goto L2 */
 
- 
-  movl  (%r12), %eax    /* eax = *r12 ou eax = *P */
-  andl  $1, %eax
-  jnz L3
+  movl  (%r12), %eax    /* eax = *r12 */
 
-  movl (%r12), %eax /* recupera o valor perdido na operacao andl */
 /*************************************************************/
-/* imprime o valor PAR de eax  */
+/* este trecho imprime o valor de %eax (estraga %eax)  */
   movq    $Sf, %rdi    /* primeiro parametro (ponteiro)*/
   movl    %eax, %esi   /* segundo parametro  (inteiro) */
   call  printf       /* chama a funcao da biblioteca */
 /*************************************************************/
 
-L3:
-  addl  $1, %ebx  /* Incrementa o contador i -> ebx += 1; */
-  addq  $4, %r12  /* Anda sobre o array de int -> r12 += 4; */ 
+  addl  $1, %ebx  /* ebx += 1; */
+  addq  $4, %r12  /* r12 += 4; */
   jmp  L1         /* goto L1; */
 
-
-
-L2: 
+L2:  
 /***************************************************************/
 /* mantenha este trecho aqui e nao mexa - finalizacao!!!!      */
   movq  $0, %rax  /* rax = 0  (valor de retorno) */
